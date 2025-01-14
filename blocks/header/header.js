18cab9649302f871b97238ed0401d9c35ffaa856
mikeippi123 <michael.ippolito3@ey.com>
2025-01-14 18:26:28 -0500

    made some mobile view updates

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -153,7 +153,7 @@ export default async function decorate(block) {
       <span class="nav-hamburger-icon"></span>
     </button>`;
   hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
-  nav.prepend(hamburger);
+  nav.appendChild(hamburger);
   nav.setAttribute('aria-expanded', 'false');
   // prevent mobile nav behavior on window resize
   toggleMenu(nav, navSections, isDesktop.matches);
