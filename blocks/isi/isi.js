8bfd2f10cdda12edd438df96eaabd0d3085ab5ab
Carson Komes <Carson.Komes+EYGS@ey.com>
2025-01-22 11:26:23 -0600

    create isi block

---
+++ blocks/isi/isi.js
@@ -0,0 +1,11 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+
+/**
+ * @param {Element} block The isi
+ * loads and decorates the isi block element
+ */
+export default async function decorate(block) {
+
+}
