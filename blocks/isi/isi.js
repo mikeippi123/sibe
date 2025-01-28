94ac04c59751cf13f4a568dd702a04155c82d1ab
Carson Komes <Carson.Komes+EYGS@ey.com>
2025-01-28 10:50:33 -0600

    Update isi.js

--- blocks/isi/isi.js
+++ blocks/isi/isi.js
@@ -4,7 +4,28 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
 
   const url = "https://publish-p7906-e91530.adobeaemcloud.com/graphql/execute.json/sibe/getAllISIText";
-  const responseJSON;
+
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({ query: `
+         query{
+          data{
+            isiTextList{
+              items{
+                _path
+                title
+                ISI_Text{
+                  html
+                }
+              }
+            }
+          }
+         }
+        `,
+    }),
+  })
+  .then((res) => res.json());
 
 
 }
