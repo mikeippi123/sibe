eacfc698a8af07f5dff76a873b910fcc716fb090
Carson Komes <Carson.Komes+EYGS@ey.com>
2025-01-28 10:36:09 -0600

    Update isi.js

---
+++ blocks/isi/isi.js
@@ -0,0 +1,10 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+
+  const url = "https://publish-p7906-e91530.adobeaemcloud.com/graphql/execute.json/sibe/getAllISIText";
+  const responseJSON;
+
+
+}
