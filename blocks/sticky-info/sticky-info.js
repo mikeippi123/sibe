c27c7140dc8eb12c9ce8df74a2f1a1a54082a2da
mikeippi123 <michael.ippolito3@ey.com>
2025-02-25 16:00:52 -0500

    sticky info block is in a good place

--- blocks/sticky-info/sticky-info.js
+++ blocks/sticky-info/sticky-info.js
@@ -3,7 +3,6 @@ export default function decorate(block) {
 
   const existingPicture = block.querySelector('picture');
   button.classList.add('sticky-info-link');
-  
   button.prepend(existingPicture);
 
   const anchor = document.querySelector('.sticky-info .button-container a');
@@ -16,8 +15,8 @@ export default function decorate(block) {
 
   span.classList.add('sticky-info-text');
 
-  button.append(span)
+  button.append(span);
 
   const picture = document.querySelector('.sticky-info .button-container picture');
-  picture.classList.add('sticky-info-picture'); 
+  picture.classList.add('sticky-info-picture');
 }
